f840ed767be63b8a43618ba1a3d80cae528c4b35
Sameer <hisameer0026@gmail.com>
2025-02-11 14:40:54 +0530

    tailwind installed

---
+++ src/Components/Initial.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const Initial = () => {
+  return (
+    <>
+    <div className='flex items-center justify-center mt-[15%]'>
+
+        <div className='bg-[#f5d5ae] rounded-2xl flex items-center justify-center p-20 font-bold text-3xl hover:scale-110 duration-700'>
+Hellooo!!!
+        </div>
+    </div>
+    
+    
+    
+    </>
+  )
+}
+
+export default Initial
